ba2b6a09588b12c18c42a031789c85ccc4b565d7
Sanduu11 <159346693+Sanduu11@users.noreply.github.com>
2025-03-11 11:31:42 +0100

    Assign data structures to variables

---
+++ basic_data_structures.py
@@ -0,0 +1,8 @@
+# Task 1 - Assigning data structures to variables
+
+my_age = 20  # integer
+my_name = "Sandu"  # string
+is_student = True  # boolean
+height = 1.60  # float
+hobbies = ["reading", "cycling", "coding"]  # list
+profile = {"name": "Sandu", "age": 20}  # dictionary
