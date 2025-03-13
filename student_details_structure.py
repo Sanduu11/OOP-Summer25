12dfa6f2db7beca99351f9fdedeef2d04f59b8cf
Sanduu11 <159346693+Sanduu11@users.noreply.github.com>
2025-03-13 12:11:25 +0100

    git add . git commit -m 'Add new file' git push origin main

---
+++ student_details_structure.py
@@ -0,0 +1,11 @@
+# student_details_structure.py
+student1 = {
+    "first_name": "Sandugash",
+    "last_name": "Nurbolat",
+    "index_number": "35176",
+    "nationality": "Kazakhstan",
+    "starting_date": "2024-09-01",
+    "courses": ["Object-oriented programming", "Math", "Logic", "English"]
+}
+
+print(student1["first_name"], student1["last_name"])
