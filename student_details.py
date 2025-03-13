e335789cfdd9189bd0ba1680ee2520249a643a12
Sanduu11 <159346693+Sanduu11@users.noreply.github.com>
2025-03-13 12:06:46 +0100

    git add student_details.py git commit -m "Add student details file" git push origin main

---
+++ student_details.py
@@ -0,0 +1,9 @@
+# student_details.py
+first_name = "Sandugash"
+last_name = "Nurbolat"
+index_number = "35176"
+nationality = "Kazakhstan"
+starting_date = "2024-09-01"
+courses = ["Object-oriented programming", "Math", "Logic", "English"]
+
+print(first_name, last_name, index_number, nationality, starting_date, courses)
