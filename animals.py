cf045bbce0bc55ce86d8ffddb7033b43d7ac88dc
Sanduu11 <159346693+Sanduu11@users.noreply.github.com>
2025-03-20 12:18:23 +0100

    Create animals.py

---
+++ animals.py
@@ -0,0 +1,37 @@
+animal1 = {
+    'name': 'Cat',
+    'group': 'mammals',
+    'number_of_legs': 4,
+    'skills': ['purring', 'climbing', 'hunting']
+}
+animal2 = {
+    'name': 'Eagle',
+    'group': 'birds',
+    'number_of_legs': 2,
+    'skills': ['flying', 'hunting', 'sharp vision']
+}
+animal3 = {
+    'name': 'Snake',
+    'group': 'reptiles',
+    'number_of_legs': 0,
+    'skills': ['slithering', 'swimming', 'venom injection']
+}
+animal4 = {
+    'name': 'Frog',
+    'group': 'amphibians',
+    'number_of_legs': 4,
+    'skills': ['jumping', 'swimming', 'camouflage']
+}
+animal5 = {
+    'name': 'Dolphin',
+    'group': 'mammals',
+    'number_of_legs': 0,
+    'skills': ['swimming', 'echolocation', 'intelligence']
+}
+
+
+print(animal1)
+print(animal2)
+print(animal3)
+print(animal4)
+print(animal5)
